f0ef9f78cf6568ce76d31e43ee757abb42a8af4b
Aadil Qureshi <36612205+aadilq@users.noreply.github.com>
2025-12-19 13:25:09 -0500

    define main function

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 
-int main() {
-  cout << "Hello World!";
-  return 0;
+int main(int argc, char* argv[]) {
+
+    if(argc != 3){
+        cout << "Provide proper usage arguments such as ./converter input.md output.html" << endl;
+        return 1;
+    }
+
+    string inputFile = argv[1];
+    string outputFile = argv[2];
+
 }
